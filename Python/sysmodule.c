f77ecfc51520a31056a566b63afbd96e3572b01a
Eric Snow <ericsnowcurrently@gmail.com>
2016-10-10 11:31:51 -0600

    Use _PyModule_Create2() for key builtin modules.

--- Python/sysmodule.c
+++ Python/sysmodule.c
@@ -1907,7 +1907,7 @@ _PySys_Init(void)
     PyObject *m, *sysdict, *version_info;
     int res;
 
-    m = PyModule_Create(&sysmodule);
+    m = _PyModule_Create2(&sysmodule, PYTHON_API_VERSION);
     if (m == NULL)
         return NULL;
     sysdict = PyModule_GetDict(m);
